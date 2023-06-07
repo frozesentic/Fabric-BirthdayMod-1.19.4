7c52f0ca6441c0cca6c4aea39c1c3fb51e868911
Cas Reehuis <cas.reehuis@collegemelle.be>
2023-06-07 23:23:14 +0200

    Initial commit

---
+++ src/main/java/net/birthdaymod/BirthdayModClient.java
@@ -0,0 +1,10 @@
+package net.birthdaymod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BirthdayModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
